64e37f47dbaa139d687cb885ebc7899fbcfc4e84
Greg Hawthorne <ghawthorne@me.com>
2020-04-11 12:27:41 -0700

    card component api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { SearchBox }from './components/search-box/search-box.component'
-import { CardList }from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css'
 
 class App extends Component {
@@ -14,8 +14,8 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
 
 
   }
@@ -23,15 +23,13 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <CardList name="Yihua">
-        <h1>Yihua</h1>
         <h1>Monsters Rolodex</h1>
-        {/* <SearchBox></SearchBox> */}
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}> { monster.name }</h1>))
+        <CardList monsters={this.state.monsters}>
+          {
+            this.state.monsters.map(monster => (
+              <h1 key={monster.id}> {monster.name}</h1>))
           }
-          </CardList>
+        </CardList>
 
       </div>
     );
