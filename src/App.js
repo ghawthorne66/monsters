649a50b005b51a962462709796028c1dff23b046
Greg Hawthorne <ghawthorne@me.com>
2020-04-11 09:24:30 -0700

    display name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Yihua Zhang'
+    }
+  }
+
   render() {
     return (
       <div>
+        <p>{this.state.string }</p>
+        <button onClick={() => this.setState({ string: 'Hello Andrei'})  }></button>
         
       </div>
     );
