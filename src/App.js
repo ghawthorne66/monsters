5424405914474b46f8c4341c5515509ba860684c
Greg Hawthorne <ghawthorne@me.com>
2020-04-11 10:06:45 -0700

    display fetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Yihua Zhang'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+
+
   }
 
   render() {
     return (
-      <div>
-        <p>{this.state.string }</p>
-        <button onClick={() => this.setState({ string: 'Hello Andrei'})  }></button>
-        
+      <div className='App'>
+        {
+          this.state.monsters.map(monster => (
+          <h1 key={monster.id}> { monster.name }</h1>))
+        }
+
       </div>
     );
   }
